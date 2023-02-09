b753cf41f0b91642f5121120a3ae3cf8f0942d80
André Campos <106924044+Andrefcampos@users.noreply.github.com>
2023-02-08 22:49:33 -0300

    Update ex008.py

--- ex008.py
+++ ex008.py
@@ -2,4 +2,4 @@ nota1 = float(input('Digite a primeira nota do aluno: '))
 nota2 = float(input('Digite a segunda nota do aluno: '))
 
 
-print('A primeira nota do aluno foi {:.1f}, \na segunda nota foi {:.1f} e a \nmédia do aluno foi {:.1f}'.format(nota1, nota2, ((nota1+nota2)/2)))
+print(f'A primeira nota do aluno foi {nota1:.1f}, \na segunda nota foi {nota2:.1f} e a \nmédia do aluno foi {(nota1+nota2)/2:.1f}')
