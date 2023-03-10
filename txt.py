421bce329080b06be3dc8020a4cd22e8d1316aa4
André Campos <106924044+Andrefcampos@users.noreply.github.com>
2023-03-09 22:56:03 -0300

    Add files via upload

---
+++ txt.py
@@ -0,0 +1,9 @@
+abc = 'oi sou gay demais porra'
+swap = abc.split()
+print(swap)
+
+words = 'His e-mail is q-lar@freecodecamp.org'
+pieces = words.split()
+parts = pieces[3].split('-')
+n = parts[1]
+print(n)
